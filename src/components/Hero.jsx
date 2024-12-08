e4690132226b45fa4f86c36b0a241ed2e6cfddbf
Muhammad Haram <haramh643@gmail.com>
2024-12-09 02:36:01 +0500

    c

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,17 +10,22 @@ const Hero = () => {
     const nextRef = useRef(null);
 
 
-    const handleMiniClick = () => {
-        
+    const handleMiniVdClick = () => {
+        setHasClicked(true)
+
+        setCurrentIndex((prevIndex) => prevIndex + 1);
+
+
+
     }
 
     return (
         <div className='relative h-dvh w-screen overflow-x-hidden'>
-            <div id='video-frame' className='relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75'>
+            <div id='video-fra me' className='relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75'>
                 <div>
                     <div className='mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg bg-black'>
 
-                        <div onClick={handleMiniClick}>
+                        <div onClick={handleMiniVdClick} className='origin-center'>
                             miniVideo
                         </div>
 
