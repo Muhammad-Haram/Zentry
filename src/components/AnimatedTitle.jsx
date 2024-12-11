9c29ef1aaa57991788d0149b47b38f1c8eb085ca
Muhammad Haram <haramh643@gmail.com>
2024-12-11 17:34:04 +0500

    animated Title

--- src/components/AnimatedTitle.jsx
+++ src/components/AnimatedTitle.jsx
@@ -39,7 +39,7 @@ const AnimatedTitle = ({ title, containerClass }) => {
         <div ref={containerRef} className={clsx("animated-title", containerClass)}>
             {title.split("<br />").map((line, index) => (
                 <div key={index} className='flex-center max-w-full flex-wrap gap-2 px-10 md:gap-3'>
-                    {line.split('').map((word, idx) => (
+                    {line.split(' ').map((word, idx) => (
                         <span
                             key={idx}
                             className="animated-word"
