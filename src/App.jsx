cfdd2110388e17942aa542291fabc02915ef4624
Muhammad Haram <haramh643@gmail.com>
2024-12-11 17:30:26 +0500

    about jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Hero from './components/Hero'
+import About from './components/About'
 
 const App = () => {
   return (
     <main>
       <Hero />
-      <section className='z-0 min-h-screen bg-blue-500' />
+      <About />
     </main>
   )
 }
